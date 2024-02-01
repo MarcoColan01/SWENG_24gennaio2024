3d871bf08efde4edd9f3f66f84c5b02c5c5dc694
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-01 15:26:08 +0100

    ROSSO: testLocalityEmpty fallisce

---
+++ src/main/java/it/unimi/di/sweng/esame/presenter/InputForecastPresenter.java
@@ -0,0 +1,14 @@
+package it.unimi.di.sweng.esame.presenter;
+
+import it.unimi.di.sweng.esame.view.InputView;
+import org.jetbrains.annotations.NotNull;
+
+public class InputForecastPresenter implements InputPresenter{
+    public InputForecastPresenter(InputView view) {
+    }
+
+    @Override
+    public void action(@NotNull String text, @NotNull String text1, @NotNull String text2) {
+        
+    }
+}
---
+++ src/test/java/it/unimi/di/sweng/esame/presenter/InputForecastPresenterTest.java
@@ -0,0 +1,19 @@
+package it.unimi.di.sweng.esame.presenter;
+
+import it.unimi.di.sweng.esame.view.InputView;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class InputForecastPresenterTest {
+
+    @Test
+    void testLocalityEmpty(){
+        InputView view = mock(InputView.class);
+        InputForecastPresenter SUT = new InputForecastPresenter(view);
+        SUT.action("", "", "");
+        verify(view).showError("empty location name");
+    }
+}
