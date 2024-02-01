1b9a70f177de7d13141a174c43512e038ce32708
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-01 17:24:21 +0100

    ROSSO: testSecondViewStrategy fallisce

--- src/test/java/it/unimi/di/sweng/esame/presenter/DisplayViewStrategyTest.java
+++ src/test/java/it/unimi/di/sweng/esame/presenter/DisplayViewStrategyTest.java
@@ -34,4 +34,28 @@ class DisplayViewStrategyTest {
         assertThat(SUT.printSegnalazioni(segnalazioni)).containsExactly("Milano THUNDERSTORM 01/05/2024",
                 "Palermo THUNDERSTORM 10/05/2024");
     }
+
+    @Test
+    void testSecondViewStrategy(){
+        DisplayViewStrategy SUT = new SecondViewStrategy();
+        List<Segnalazione> segnalazioni = new ArrayList<>();
+        Segnalazione s1 = new Segnalazione("Milano",
+                Criticity.THUNDERSTORM,
+                Data.creaData("02/05/2024"));
+        Segnalazione s2 = new Segnalazione("Milano",
+                Criticity.THUNDERSTORM,
+                Data.creaData("01/05/2024"));
+        Segnalazione s3 = new Segnalazione("Palermo",
+                Criticity.THUNDERSTORM,
+                Data.creaData("10/05/2024"));
+        Segnalazione s4 = new Segnalazione("Palermo",
+                Criticity.THUNDERSTORM,
+                Data.creaData("17/05/2024"));
+        segnalazioni.add(s1);
+        segnalazioni.add(s2);
+        segnalazioni.add(s3);
+        segnalazioni.add(s4);
+        SUT.sortSegnalazioni(segnalazioni);
+        assertThat(segnalazioni).containsExactly(s2,s1,s3,s4);
+    }
 }
