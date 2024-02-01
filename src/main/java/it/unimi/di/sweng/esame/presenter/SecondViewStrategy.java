d0837e804db87b9742dda5e2e63733c6f49566f5
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-01 17:25:30 +0100

    VERDE: testSecondViewStrategy ora funziona

--- src/main/java/it/unimi/di/sweng/esame/presenter/SecondViewStrategy.java
+++ src/main/java/it/unimi/di/sweng/esame/presenter/SecondViewStrategy.java
@@ -2,16 +2,20 @@ package it.unimi.di.sweng.esame.presenter;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
+import java.util.*;
 
 public class SecondViewStrategy implements DisplayViewStrategy {
     @Override
     public void sortSegnalazioni(@NotNull List<Segnalazione> segnalazioni) {
-
+        segnalazioni.sort(Comparator.comparing(Segnalazione::data));
     }
 
     @Override
     public List<String> printSegnalazioni(List<Segnalazione> segnalazioni) {
-        return null;
+        List<String> s = new ArrayList<>();
+        for (Segnalazione segnalazione : segnalazioni) {
+            s.add(segnalazione.nomeCitta() + " " + segnalazione.criticity() + " " + segnalazione.data());
+        }
+        return s;
     }
 }
