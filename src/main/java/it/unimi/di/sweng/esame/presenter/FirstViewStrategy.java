88113c392e4a7ee980b98d04ed7a437b4a6e0c2d
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-01 17:22:31 +0100

    VERDE: testFirstViewStrategy ora funziona

--- src/main/java/it/unimi/di/sweng/esame/presenter/FirstViewStrategy.java
+++ src/main/java/it/unimi/di/sweng/esame/presenter/FirstViewStrategy.java
@@ -2,16 +2,24 @@ package it.unimi.di.sweng.esame.presenter;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
+import java.util.*;
 
 public class FirstViewStrategy implements DisplayViewStrategy {
     @Override
     public void sortSegnalazioni(@NotNull List<Segnalazione> segnalazioni) {
-
+        segnalazioni.sort(Comparator.comparing(Segnalazione::data));
     }
 
     @Override
-    public List<String> printSegnalazioni(List<Segnalazione> segnalazioni) {
-        return null;
+    public List<String> printSegnalazioni(@NotNull List<Segnalazione> segnalazioni) {
+        List<String> s = new ArrayList<>();
+        Map<String, Boolean> citta = new HashMap<>();
+        for (Segnalazione segnalazione : segnalazioni) {
+            if(!citta.containsKey(segnalazione.nomeCitta())){
+                s.add(segnalazione.nomeCitta() + " " + segnalazione.criticity() + " " + segnalazione.data());
+                citta.put(segnalazione.nomeCitta(), true);
+            }
+        }
+        return s;
     }
 }
