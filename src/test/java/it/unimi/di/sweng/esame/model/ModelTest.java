9392f11e5d950add097ae320412a5197ae79d62c
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-01 16:58:54 +0100

    VERDE DIRETTO: testAddPiuSegnalazioniReplace funziona

--- src/test/java/it/unimi/di/sweng/esame/model/ModelTest.java
+++ src/test/java/it/unimi/di/sweng/esame/model/ModelTest.java
@@ -32,4 +32,14 @@ class ModelTest {
         SUT.addSegnalazione(new Segnalazione("Milano", Criticity.HURRICANE, Data.creaData("02/05/2024")));
         assertThat(SUT.getSegnalazioni("Milano")).hasSize(1);
     }
+
+    @Test
+    void testAddPiuSegnalazioniReplace(){
+        Model SUT = new Model();
+        SUT.addSegnalazione(new Segnalazione("Milano", Criticity.RAINY, Data.creaData("02/05/2024")));
+        SUT.addSegnalazione(new Segnalazione("Genova", Criticity.RAINY, Data.creaData("01/05/2024")));
+        SUT.addSegnalazione(new Segnalazione("Padova", Criticity.THUNDERSTORM, Data.creaData("02/05/2024")));
+        SUT.addSegnalazione(new Segnalazione("Milano", Criticity.HURRICANE, Data.creaData("02/05/2024")));
+        assertThat(SUT.getSegnalazioni()).hasSize(3);
+    }
 }
